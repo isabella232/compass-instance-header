f13237b9ca203e17fe2301f6e12e6710359a3b46
aherlihy <anna.herlihy@10gen.com>
2019-02-15 18:03:23 +0100

    Add component test

--- src/components/instance-header/instance-header.spec.js
+++ src/components/instance-header/instance-header.spec.js
@@ -19,8 +19,24 @@ describe('InstanceHeader [Component]', () => {
     it('renders the correct root classname', () => {
       expect(component.find(`.${styles['instance-header']}`)).to.be.present();
     });
+    it('renders one item on the left', () => {
+      expect(component.find(`.${styles['instance-header-items-is-left']}`).children()).to.have.lengthOf(1);
+    });
+  });
+  describe('collapsed', () => {
+    beforeEach(() => {
+      component = mount(<InstanceHeader sidebarCollapsed name="name" activeNamespace="active namespace"/>);
+    });
+
+    afterEach(() => {
+      component = null;
+    });
+
+    it('renders the correct root classname', () => {
+      expect(component.find(`.${styles['instance-header']}`)).to.be.present();
+    });
     it('renders as expanded', () => {
-      // expect(component.find(`.${styles['instance-header-sidebar-expanded']}`)).to.be.present();
+      expect(component.find(`.${styles['instance-header-sidebar-collapsed']}`)).to.be.present();
     });
     it('renders one item on the left', () => {
       expect(component.find(`.${styles['instance-header-items-is-left']}`).children()).to.have.lengthOf(1);
